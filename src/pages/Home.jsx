d7e3fce2fcd73f454079374f9398806e918479c8
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-05-30 19:13:06 -0400

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/post`, {
+        const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/api/v1/post`, {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json'
