c8462fb364342cc40643c75c397ce7f04636719f
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-06-01 13:59:21 -0400

    Chat menu functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Home, CreatePost } from './pages'
 import Chat from './pages/Chat'
 
 const App = () => {
+
   return (
     <BrowserRouter>
       <header
@@ -25,7 +26,7 @@ const App = () => {
 
       </header>
 
-      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)] relative'>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-post" element={<CreatePost />} />
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FormField } from '../components'
+import { useLocation } from 'react-router-dom';
+import ChatMenu from '../components/ChatMenu';
 
 const Chat = () => {
+    const location = useLocation();
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const [allChats, setAllChats] = useState([]);
     const [userText, setUserText] = useState("");
     const [generating, setGenerating] = useState(false);
     const [responseData, setResponseData] = useState([
@@ -37,6 +44,12 @@ const Chat = () => {
                         prompt: userText,
                         response: data.response.choices[0].message.content
                     }])
+
+                    setAllChats(prevChats => [
+                        data.newChat,
+                    ...prevChats ])
+
+                    console.log(data)
                 }
             } catch (error) {
                 alert("Error in response from Chat-GPT");
@@ -49,56 +62,88 @@ const Chat = () => {
 
     }
 
+    useEffect(() => {
+        const getChats = async () => {
+            try {
+                const response = await fetch("http://localhost:5555/api/v1/chat", {
+                  method: 'GET',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  }
+                })
+        
+                if (response.ok) {
+                  const result = await response.json();
+
+                  setAllChats(result.data.reverse());
+                }
+              } catch (error) {
+                alert(error);
+              }
+        }
+
+        getChats();
+    }, [])
+
+    const clearChat = () => {
+        setResponseData([]);
+    }
+
     return (
-        <div className='h-full min-h-[calc(100vh-130px)] max-w-7xl flex flex-col justify-between  mx-auto p-4'>
-            <div className="flex-grow">
-                <h2 className="text-2xl font-bold mb-4 text-center">Ask Chat-GPT</h2>
-                {responseData && responseData.map(res => (
-                    <>
-                        <p className='inline-block bg-[#e8e8e8] p-4 rounded mb-4 text-left justify-end'>
-                            {res.prompt}
-                        </p>
-                        <p className="mb-4 max-w-3xl mx-auto text-left px-4">
-                            {res.response}
-                        </p>
-                    </>
+        <div className='flex w-full'>
+            <ChatMenu chats={allChats} clearChat={clearChat} isOpen={isOpen} setIsOpen={setIsOpen} />
+            <div className={`w-full ${isOpen ? 'sm:pl-[200px]' : 'sm:pl-0'} h-full min-h-[calc(100vh-130px)] mx-auto max-w-5xl flex flex-col justify-between p-4`}>
 
-                ))}
+                <div className="flex-grow">
+                    <h2 className="text-2xl font-bold mb-4 text-center">Ask Chat-GPT</h2>
+                    {responseData && responseData.map(res => (
+                        <>
+                            <p className='inline-block bg-[#f1f1f1] p-4 rounded mb-4 text-left justify-end'>
+                                {res.prompt}
+                            </p>
+                            <p className="mb-4 max-w-3xl text-left px-5">
+                                {res.response}
+                            </p>
+                        </>
+
+                    ))}
+                </div>
+                <div className='mb-4'>
+                    <FormField
+                        LabelName="Ask"
+                        type="text"
+                        name="userText"
+                        placeholder="e.g. Fun fact about the Roman Empire"
+                        value={userText}
+                        handleChange={handleChange}
+                    />
+                    {generating ? (
+                        <div className='flex items-center justify-center'>
+                            <button className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md mt-2 self-center'>
+                                Generating...
+                            </button>
+                        </div>
+                    ) : (
+                        <div className='flex items-center justify-center'>
+                            <button
+                                className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md mt-2 self-center'
+                                type="submit"
+                                onClick={handleSubmit}
+                            >
+                                Generate
+                            </button>
+                        </div>
+                    )}
+
+
+                    <p className='text-sm mt-2 text-center'>
+                        ChatGPT can make mistakes. Check important info.
+                    </p>
+                </div>
+
             </div>
-            <div className='mb-4'>
-                <FormField
-                    LabelName="Ask"
-                    type="text"
-                    name="userText"
-                    placeholder="e.g. Fun fact about the Roman Empire"
-                    value={userText}
-                    handleChange={handleChange}
-                />
-                {generating ? (
-                    <div className='flex items-center justify-center'>
-                        <button className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md mt-2 self-center'>
-                            Generating...
-                        </button>
-                    </div>
-                ) : (
-                    <div className='flex items-center justify-center'>
-                        <button
-                            className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md mt-2 self-center'
-                            type="submit"
-                            onClick={handleSubmit}
-                        >
-                            Generate
-                        </button>
-                    </div>
-                )}
-
-
-                <p className='text-sm mt-2 text-center'>
-                    ChatGPT can make mistakes. Check important info.
-                </p>
-            </div>
-
         </div>
+
     )
 }
 
