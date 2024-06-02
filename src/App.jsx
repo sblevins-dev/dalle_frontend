48774559dfabcd4e32b56a23b3034a3fc9f8fb1b
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-06-02 12:30:51 -0400

    changed browser to hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
 import { Home, CreatePost } from './pages'
 import Chat from './pages/Chat'
@@ -8,7 +8,7 @@ import Navbar from './components/Navbar'
 const App = () => {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
 
       <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)] relative'>
@@ -18,7 +18,7 @@ const App = () => {
           <Route path="/chat" element={<Chat />} />
         </Routes>
       </main>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
